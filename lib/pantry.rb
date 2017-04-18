8c075fb6429b70d3287716467d56122b86dbec28
Sam Lim <limsammy1@gmail.com>
2017-04-18 10:45:36 -0600

    wrote add_to_cookbook function, created cookbook array that contains recipes

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,4 +1,11 @@
 class Pantry
+  attr_reader :cookbook
+
   def initialize
+    @cookbook = []
+  end
+
+  def add_to_cookbook(recipe)
+    cookbook << recipe
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -5,20 +5,26 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
   def setup
-    r1 = Recipe.new("Cheese Pizza")
-    r1.add_ingredient("Cheese", 20)
-    r1.add_ingredient("Flour", 20)
+    @r1 = Recipe.new("Cheese Pizza")
+    @r1.add_ingredient("Cheese", 20)
+    @r1.add_ingredient("Flour", 20)
 
-    r2 = Recipe.new("Pickles")
-    r2.add_ingredient("Brine", 10)
-    r2.add_ingredient("Cucumbers", 30)
+    @r2 = Recipe.new("Pickles")
+    @r2.add_ingredient("Brine", 10)
+    @r2.add_ingredient("Cucumbers", 30)
 
-    r3 = Recipe.new("Peanuts")
-    r3.add_ingredient("Raw nuts", 10)
-    r3.add_ingredient("Salt", 10)
+    @r3 = Recipe.new("Peanuts")
+    @r3.add_ingredient("Raw nuts", 10)
+    @r3.add_ingredient("Salt", 10)
   end
   def test_it_exists
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_can_add_to_cookbook
+    pantry = Pantry.new
+    pantry.add_to_cookbook(@r1)
+    assert_equal 1, pantry.cookbook.count
   end
 end
