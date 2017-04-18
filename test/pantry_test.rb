9bb7206c3f780b53f85beadee75afc9fb8c534f6
Sam Lim <limsammy1@gmail.com>
2017-04-18 10:42:36 -0600

    added recipe class to pantry test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,8 +1,22 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def setup
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+  end
   def test_it_exists
     pantry = Pantry.new
     assert_instance_of Pantry, pantry
