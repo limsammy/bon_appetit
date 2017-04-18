61aec8c3b13c8a216f77260402f931008bf8992b
Sam Lim <limsammy1@gmail.com>
2017-04-18 10:41:45 -0600

    Added first test to pantry, it exists

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
-
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
 end
